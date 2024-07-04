9baebd4a45e1718459e80077588acbc6aa5b22b6
Johnnypham2 <phammjohnny@gmail.com>
2024-07-03 18:16:31 -0700

    change

--- API/Data/AppDbContext.cs
+++ API/Data/AppDbContext.cs
@@ -10,9 +10,9 @@ namespace API.Data
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
-        {
-
+        {   
         }
         public DbSet<Product> Products { get; set; }
+        
     }
 }
